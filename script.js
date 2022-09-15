d34fe7a29415fedb84faf2d07a835701bb47fc1f
sifatullahsu <personal.sifat@gmail.com>
2022-09-15 09:51:59 +0600

    Error count fixed

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
+    console.log(errorCount);
   }
 
   // check if given question text is equal to user typed text
